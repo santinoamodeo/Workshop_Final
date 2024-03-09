56fc45654e94b6c95c84a062b5e89a5650983208
Santino Amodeo <santinoamodeoph@gmail.com>
2024-03-09 00:51:40 -0300

    4

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { CartContext } from '../context/CartContext';
 import { useForm } from 'react-hook-form';
 import { collection, addDoc } from "firebase/firestore";
 import { db } from '../firebase/config';
+import { Link } from 'react-router-dom';
 
 const Checkout = () => {
 
@@ -33,25 +34,29 @@ const Checkout = () => {
 
     if (pedidoId) {
         return (
-            <div className="container">
-                <h1 className="main-title">Muchas gracias por tu compra</h1>
-                <p>Tu número de pedido es: {pedidoId}</p>
+            <div className="container_greets">
+                <h1 className="main-title">Muchas gracias por tu compra!</h1>
+                <h4>Tu orden es:</h4>
+                <h3>{pedidoId}</h3>
+                <img src="https://i.pinimg.com/originals/52/0e/52/520e5212080d0fe14d1d9d2e766889e5.gif" alt="" />
+                <li><Link  to="/">Volver al Inicio</Link></li>
             </div>
         )
     }
 
   return (
-    <div className="container">
-        <h1 className="main-title">Finalizar compra</h1>
-        <form className="formulario" onSubmit={handleSubmit(comprar)}>
+    <div className="checkout_container">
+        <div className='div_in_div'>
+            <h1 className="main-title">Finalizar compra</h1>
 
-            <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
-            <input type="email" placeholder="Ingresá tu e-mail" {...register("email")} />
-            <input type="phone" placeholder="Ingresá tu teléfono" {...register("telefono")} />
-
-            <button className="enviar" type="submit">Comprar</button>
-
-        </form>
+            <form className="formulario" onSubmit={handleSubmit(comprar)}>
+                <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
+                <input type="email" placeholder="Ingresá tu e-mail" {...register("email")} />
+                <input type="phone" placeholder="Ingresá tu teléfono" {...register("telefono")} />
+                <button className="enviar" type="submit">Comprar</button>
+            </form>
+    
+        </div>
     </div>
   )
 }
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -10,17 +10,15 @@ const Contacto = () => {
 
   return (
 
-    <div className="container contacto_container">
+    <div className="contacto_container">
       <div className="div_in_div">
         <h1 className="main-title">Contacto</h1>
         <form className="formulario" onSubmit={handleSubmit(enviar)}>
-
             <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
             <input type="email" placeholder="Ingresá tu e-mail" {...register("email")} />
             <input type="phone" placeholder="Ingresá tu teléfono" {...register("telefono")} />
-
+            <button className="enviar" type="submit">Enviar</button>
         </form>
-        <button className="enviar" type="submit">Enviar</button>
       </div>
     </div>
 
