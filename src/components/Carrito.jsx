26aefe2cf2023c332db334c7b5d90e5b98252a1c
Santino Amodeo <santinoamodeoph@gmail.com>
2024-03-09 00:19:02 -0300

    3

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -13,30 +13,37 @@ const Carrito = () => {
   return (
     
         <div className='carrito_container'>
-            <h1 className="main-title">Carrito</h1>
+            <div className='cuadrante_carrito'>
+                <div className='aas'>
+                <h1 className="main-title">Carrito</h1>    
+                </div>
+                
 
-            {   
-                carrito.map((prod) => (
-                    <div key={prod.id}>
-                        <br />
-                        <h3>{prod.titulo}</h3>
-                        <p>Precio unit: ${prod.precio}</p>
-                        <p>Precio total: ${prod.precio * prod.cantidad}</p>
-                        <p>Cant: {prod.cantidad}</p>
-                        <br />
-                    </div>
-                ))
-            }
+                {   
+                    carrito.map((prod) => (
+                        <div className='datos1' key={prod.id}>
+                            <br />
+                            <h3>{prod.titulo}</h3>
+                            <p>Cantidad: {prod.cantidad}</p>
+                            <p>Precio unitario: ${prod.precio}</p>
+                            <p>Precio total: ${prod.precio * prod.cantidad}</p>
+                            <br />
+                        </div>
+                    ))
+                }
 
-            {  
-                carrito.length > 0 ?
-                <>
-                    <h2>Precio total: ${precioTotal()}</h2>
-                    <button onClick={handleVaciar}>Vaciar</button>
-                    <Link to="/checkout">Finalizar Compra</Link>
-                </> :
-                <h2>El carrito está vacío :(</h2>
-            }
+                {  
+                    carrito.length > 0 ?
+                    <div className='datos2'>
+                        <h2>Precio total: ${precioTotal()}</h2>
+                        <div className='button_container'>
+                            <button className='vaciar' onClick={handleVaciar}>Vaciar</button>
+                            <Link className='finalizar_compra' to="/checkout">Finalizar Compra</Link>
+                        </div>
+                    </div> :
+                    <h2>El carrito está vacío aún! Llenalo!</h2>
+                }
+            </div>        
         </div>
     
   )
